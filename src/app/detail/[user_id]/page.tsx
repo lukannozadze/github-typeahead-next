19d7dec724f1d2fc3eeb8bc1db1f18209e1ff534
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-03-18 15:51:22 +0400

    back button

--- src/app/detail/[user_id]/page.tsx
+++ src/app/detail/[user_id]/page.tsx
@@ -1,10 +1,14 @@
 import UserInfo from "@/app/components/detail/UserInfo";
 import Wrapper from "@/app/components/detail/Wrapper";
+import BackButton from "@/app/ui/BackButton";
 
 export default function page() {
   return (
+    <>
+     <BackButton/>
     <Wrapper>
        <UserInfo/>
     </Wrapper>
+    </>
   )
 }
---
+++ src/app/ui/BackButton.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+Link
+
+function BackButton() {
+  return (
+    <Link href='/'>
+   <button className=" absolute top-8 left-8 z-50 px-10 py-2 rounded-full bg-green-700 font-bold text-white tracking-widest uppercase transform hover:scale-105 hover:bg-[#21e065] transition-colors duration-200">
+   Back
+</button>
+
+
+    </Link>
+  )
+}
+
+export default BackButton
